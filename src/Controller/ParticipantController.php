fc39c319a067768ec553670d09f31ca4be9048c8
unknown <Administrateur@27SE6-94NZG5J.ad.campus-eni.fr>
2020-02-10 14:57:52 +0100

    ajout page modifier profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -7,7 +7,6 @@ use App\Form\ParticipantType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,7 +22,7 @@ class ParticipantController extends AbstractController
 
         //récupère tout mon enregistrement :
         $participantExistant = $participantRepository->find($id);
-        $form = $this->createForm(IdeaType::class,  $participantExistant);
+        $form = $this->createForm(ParticipantType::class,  $participantExistant);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -31,12 +30,12 @@ class ParticipantController extends AbstractController
             //    $em->persist($ideaExistantes);
             $em->flush();
 
-            $this->addFlash('success', 'Souhait modifié !');
-            return $this->redirectToRoute('list');
+            $this->addFlash('success', 'profil modifié !');
+            return $this->redirectToRoute('home');
         }
 
-        return $this->render('idea/modifier.html.twig', [
-            'form' => $form->createView()
+        return $this->render('profil/modifier.html.twig', [
+            'formParticipant' => $form->createView()
         ]);
     }
 }
