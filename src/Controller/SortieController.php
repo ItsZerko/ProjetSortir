cd716945b2ecd32433a36948df0785364a0b401e
unknown <Administrateur@27SE6-64NZG5J.ad.campus-eni.fr>
2020-02-11 16:28:09 +0100

    vers

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -7,6 +7,7 @@ use App\Entity\Sortie;
 use App\Form\SortieFormType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,11 +16,18 @@ class SortieController extends AbstractController
 {
     /**
      * @Route("/sortie", name="sortie")
+     * @param Request $request
+     * @return Response
      */
     public function index()
     {
-        $sortie =  new Sortie();
+
+        $test ="WTF";
+      //  dump($test);die();
+    $sortie =  new Sortie();
         $form = $this->createForm(SortieFormType ::class, $sortie);
+
+
         return $this->render('base/sortie.html.twig', [
             'controller_name' => 'SortieController',
              'sortieForm'=> $form->createView()
--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -5,9 +5,6 @@ namespace App\Form;
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,13 +14,14 @@ class SortieFormType extends AbstractType
     {
         $builder
             ->add('nom')
-
-
-
+            ->add('dateHeureDebut', DateType::class, [
+                'widget' => 'single_text'
+            ])
             ->add('duree')
-
-
-
+            ->add('dateLimiteInscription', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('nbInscriptionMax')
             ->add('infoSortie')
 
         ;
